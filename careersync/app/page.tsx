9a1d8d35ba028ca8109dbd7098282ae330e5bd64
maki-sig <mnbotis.nhcs@gmail.com>
2026-03-10 01:25:27 +0800

    Added the html skeletal structure and css for program page

--- careersync/app/page.tsx
+++ careersync/app/page.tsx
@@ -1,8 +1,25 @@
+import "../app/styles/page.css"
+
 export default function Landing(){
   return(
     <>
-      <h1>Hello Kinaban</h1>
-      <span>Pull request test</span>
+      <div className="program-toggle-div"> 
+        <span>svg here</span>
+      </div> 
+      <section className="program-section"> 
+        <h1>CareerSync</h1> 
+        <span>This  is a decision support form based web app that will align IT and CS students to related industry careers. Powered by Gemini.</span>
+        {/* the card */}
+        <div className="program-card"> 
+          <span>Choose your program to start</span>
+          {/* btn grp */}
+          <div>
+            <button>Computer Science</button>
+            <button>Information Technology</button>
+          </div> 
+          <span>Developed by: Marc Botis</span> 
+        </div>
+      </section> 
     </>
   )
 }
