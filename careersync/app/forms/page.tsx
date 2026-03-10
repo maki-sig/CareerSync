b000b13609063bd33478c84d7804a5a765ed28c9
maki-sig <mnbotis.nhcs@gmail.com>
2026-03-10 17:33:08 +0800

    Initial progress on subject category

--- careersync/app/forms/page.tsx
+++ careersync/app/forms/page.tsx
@@ -6,10 +6,10 @@ export default function Forms() {
             <Header />
 
             <form className="forms">
-                <div className="workstyle-section">
+
+                {/* <div className="workstyle-section">
                     <h1 className="category-txt">Workflow Mode</h1>
                     <div className="workstyle-card">
-                        
                         <span className="question-txt">How do you achieve your peak performance?</span>
                         <span className="caption-txt">Choose the work environment where you feel most in the zone</span>
                         
@@ -17,11 +17,25 @@ export default function Forms() {
                             <label className="btn-txt"><input type="radio" name="workstyle" />Collaborative</label>
                             <label className="btn-txt"><input type="radio" name="workstyle" />Independent</label>
                         </div>
-
                     </div>
                     <button type="button" className="secondary-btn btn-txt">
                         Next
                     </button>
+                </div> */}
+
+                <div className="subjects-section">
+                    <h1 className="category-txt">Core Domains</h1>
+                    <div className="subjects-card">
+
+                        <span className="question-txt">What technical subjects have you mastered or enjoyed most?</span>
+                        <span className="caption-txt">Select the areas where you feel most confident or find the highest level of interest</span>
+
+                    </div>
+
+                    <div className="subjects-secbtn-grp">
+                        <button type="button" className="secondary-btn btn-txt">Back</button>
+                        <button type="button" className="secondary-btn btn-txt">Next</button>
+                    </div>
                 </div>
             </form>
         </>
