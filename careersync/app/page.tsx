cf2a6cadbf942d841952553fdb3c816a417f448c
maki-sig <mnbotis.nhcs@gmail.com>
2026-03-10 10:55:01 +0800

    Landing page responsive and styled, no SVGs yet

--- careersync/app/page.tsx
+++ careersync/app/page.tsx
@@ -8,15 +8,12 @@ export default function Landing(){
       </div> 
       <section className="program-section"> 
         <h1 className="title-txt">CareerSync</h1> 
-        <span className="option-txt">This  is a decision support form based web app that will align IT and CS students to related industry careers. Powered by Gemini.</span>
+        <span className="desc option-txt">This  is a decision support form based web app that will align IT and CS students to related industry careers.<br></br>Powered by Gemini.</span>
         {/* the card */}
         <div className="program-card"> 
           <span className="caption-txt">Choose your program to start</span>
-          {/* btn grp */}
-          <div>
-            <button className="btn-txt">Computer Science</button>
-            <button className="btn-txt">Information Technology</button>
-          </div> 
+          <button className="btn-txt">Computer Science</button>
+          <button className="btn-txt">Information Technology</button> 
           <span className="caption-txt">Developed by: Marc Botis</span> 
         </div>
       </section> 
