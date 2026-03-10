d49e6f34cfd81d37dabe96f83fa44f0a5780e034
maki-sig <mnbotis.nhcs@gmail.com>
2026-03-10 09:39:15 +0800

    Fonts css completion

--- careersync/app/page.tsx
+++ careersync/app/page.tsx
@@ -7,17 +7,17 @@ export default function Landing(){
         <span>svg here</span>
       </div> 
       <section className="program-section"> 
-        <h1>CareerSync</h1> 
-        <span>This  is a decision support form based web app that will align IT and CS students to related industry careers. Powered by Gemini.</span>
+        <h1 className="title-txt">CareerSync</h1> 
+        <span className="option-txt">This  is a decision support form based web app that will align IT and CS students to related industry careers. Powered by Gemini.</span>
         {/* the card */}
         <div className="program-card"> 
-          <span>Choose your program to start</span>
+          <span className="caption-txt">Choose your program to start</span>
           {/* btn grp */}
           <div>
-            <button>Computer Science</button>
-            <button>Information Technology</button>
+            <button className="btn-txt">Computer Science</button>
+            <button className="btn-txt">Information Technology</button>
           </div> 
-          <span>Developed by: Marc Botis</span> 
+          <span className="caption-txt">Developed by: Marc Botis</span> 
         </div>
       </section> 
     </>
