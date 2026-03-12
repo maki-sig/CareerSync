bce729e63e59c0c6591934f076e07df4b2264b83
maki-sig <mnbotis.nhcs@gmail.com>
2026-03-12 11:36:42 +0800

    v1.0.4 - Deployment

--- careersync/next.config.ts
+++ careersync/next.config.ts
@@ -4,13 +4,19 @@ import path from "path";
 const nextConfig: NextConfig = {
   devIndicators: false,
   turbopack: {
-    root: path.resolve(__dirname),
     rules: {
       "*.svg": {
         loaders: ["@svgr/webpack"],
         as: "*.js",
       },
     },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
 };
 
